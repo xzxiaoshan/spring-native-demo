9ef487ef11c7867c3ca89b21c43243ada332df75
shanhy <xz.xiaoshan@gmail.com>
2022-07-26 11:00:02 +0800

    init2

--- src/main/java/com/example/springnative/config/TestConfig.java
+++ src/main/java/com/example/springnative/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.example.springnative.config;
 
 import com.example.springnative.model.Student;
 import com.example.springnative.model.Teacher;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,18 +10,19 @@ import org.springframework.context.annotation.Configuration;
  * @author shanhy
  * @date 2022-07-08 11:30
  */
+@Slf4j
 @Configuration(proxyBeanMethods = true)
 public class TestConfig {
 
     @Bean
     public Student getStudent() {
-        System.out.println("同学你好...");
+        log.info("同学你好...");
         return new Student();
     }
 
     @Bean
     public Teacher getTeacher() {
-        System.out.println("getTeacher() 方法被执行...");
+        log.info("getTeacher() 方法被执行...");
         return new Teacher(getStudent());
     }
 
